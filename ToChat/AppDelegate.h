baf1d5db3567ea1823569f49cac46c3cd2974518
JFT0M <377632523@qq.com>
2015-08-10 22:08:13 +0800

    Initial Commit

---
+++ ToChat/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ToChat
+//
+//  Created by syfll on 15/8/10.
+//  Copyright (c) 2015年 JFT0M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ToChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToChat
+//
+//  Created by syfll on 15/8/10.
+//  Copyright (c) 2015年 JFT0M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
